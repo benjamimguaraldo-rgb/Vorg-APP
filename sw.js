d407d42c1db8ad8e2917ff4502871f026d52e169
rosnante <benjamimguaraldo@gmail.com>
2026-04-05 21:59:01 -0300

    a

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'vorg-web-v1';
+const ASSETS = [
+  '/home.html',
+  '/comprar.html',
+  '/global.css',
+  '/manifest.json'
+];
+
+// Instala e faz cache dos arquivos principais
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Limpa caches antigos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Serve do cache, cai na rede se não tiver
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).catch(() => cached);
+    })
+  );
+});
